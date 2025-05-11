4714513e7d43a61dfa85c422e8223fcf2741a61b
Retual004 <foksplay04@gmail.com>
2025-05-11 22:06:13 +0400

    done 3 task

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Retual004/tasks-service/internal/database"
+	"github.com/Retual004/tasks-service/internal/task"
+	transportgrpc "github.com/Retual004/tasks-service/internal/transport/grpc"
+)
+func main() {
+	addr:= ":50052"
+	database.InitDB()
+
+	repo:= task.NewRepository(database.DB)
+	svc:= task.NewService(repo)
+ 	
+	userClient, conn, err := transportgrpc.NewUserClient("localhost:50051")
+  if err != nil {
+    log.Fatalf("failed to connect to users: %v", err)
+  }
+  defer conn.Close()
+
+  fmt.Printf("gRPC server listening on %s\n", addr)
+    // 4. Запуск gRPC Tasks-сервиса
+  if err := transportgrpc.RunGRPC(svc, userClient); err != nil {
+    log.Fatalf("Tasks gRPC server error: %v", err)
+  }
+}
